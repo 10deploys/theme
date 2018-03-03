d3ac4caf1b95c1e20e80709f1f39ff602020acb8
Eriksen Costa <eriksencosta@gmail.com>
2018-03-03 20:03:41 -0300

    Mark the active item based on player's progress
    This will help the visitor to know in which episode's part she/he is
    listening.

--- assets/js/episode.js
+++ assets/js/episode.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
   var soundCloud = SC.Widget("episode-soundcloud");
+  var skipTimes = {};
 
-  calculateSkipTime();
+  calculateSkipTime(skipTimes);
   bindSkipLinks(soundCloud);
   skipWhenQueryParamIsAvailable();
 
@@ -10,17 +11,28 @@ $(document).ready(function() {
     skipWhenQueryParamIsAvailable();
   });
 
+  soundCloud.bind(SC.Widget.Events.PLAY_PROGRESS, function(event) {
+    var currentPosition = Math.round(event.currentPosition / 1000);
+    var skipLink = skipLinkWithSeekTimeGreaterThan(skipTimes, currentPosition);
+
+    if (skipLink !== undefined) {
+      addActiveClassToSkipLink(skipLink);
+    }
+  });
+
   window.onpopstate = function(event) {
     skipWhenQueryParamIsAvailable();
   };
 });
 
-var calculateSkipTime = function() {
+var calculateSkipTime = function(skipTimes) {
   skipLinks().each(function() {
+    var index = $(this).data("index");
     var instant = $(this).data("instant");
+    var linkText = $(this).text().trim();
 
     if (instant == undefined) {
-      alert("Link without instant attribute: " + $(this).text());
+      alert("Link without instant attribute: " + linkText);
 
       return;
     }
@@ -28,7 +40,7 @@ var calculateSkipTime = function() {
     var instantParts = instant.split(":").reverse();
 
     if (instantParts.length > 3 || instantParts.length == 0) {
-      alert("Link with invalid instant: " + $(this).text());
+      alert("Link with invalid instant: " + linkText);
 
       return;
     }
@@ -38,13 +50,13 @@ var calculateSkipTime = function() {
     var seconds = castToInteger(instantParts[0]);
 
     if (!isValidTimePeriod(minutes)) {
-      alert("Link with invalid instant minutes: " + $(this).text());
+      alert("Link with invalid instant minutes: " + linkText);
 
       return;
     }
 
     if (!isValidTimePeriod(seconds)) {
-      alert("Link with invalid instant seconds: " + $(this).text());
+      alert("Link with invalid instant seconds: " + linkText);
 
       return;
     }
@@ -56,6 +68,7 @@ var calculateSkipTime = function() {
     );
 
     $(this).attr("data-seek", seek);
+    skipTimes[seek] = index;
   });
 };
 
@@ -63,8 +76,7 @@ var bindSkipLinks = function(soundCloud) {
   skipLinks().click(function(event) {
     event.preventDefault();
 
-    skipLinkWithActiveClass().removeClass("active");
-    $(this).addClass("active");
+    addActiveClassToSkipLink($(this));
 
     var seek = $(this).attr("data-seek") * 1000;
 
@@ -79,6 +91,11 @@ var bindSkipLinks = function(soundCloud) {
       history.pushState({}, "", historyPath);
     }
   });
+};
+
+var addActiveClassToSkipLink = function(skipLink) {
+  skipLinkWithActiveClass().removeClass("active");
+  skipLink.addClass("active");
 };
 
 var skipWhenQueryParamIsAvailable = function() {
@@ -109,6 +126,18 @@ var skipLinks = function() {
 
 var skipLinkOfIndex = function(index) {
   return $(skipLinkClass() + "[data-index=" + index + "]");
+};
+
+var skipLinkWithSeekTimeGreaterThan = function(skipTimes, milliseconds) {
+  var matchedSkipTimes = [];
+
+  $.each(skipTimes, function(seek, index) {
+    if (milliseconds >= castToInteger(seek)) {
+      matchedSkipTimes.push(index);
+    }
+  });
+
+  return skipLinkOfIndex(matchedSkipTimes.pop());
 };
 
 var skipLinkWithActiveClass = function() {
